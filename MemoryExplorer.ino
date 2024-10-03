b5ac622f57759b08a4a97b5d85f005fd0cbde5f2
Ian <ian@somewhere.com>
2024-10-03 14:00:01 +0200

    [~] Sync with current libraries.

--- MemoryExplorer.ino
+++ MemoryExplorer.ino
@@ -1,8 +1,8 @@
 // Simple KSP-like craft to transfer all memory contents via Serial
 
 /*
-  Memory in AVR is registers (first 32 bytes) then state of board
-  (until byte 256) then user memory (2048 bytes).
+  Memory in Arduino Uno (AVR arch) is generic registers (first 32 bytes),
+  state registers (until byte 256) then user memory (2048 bytes).
 
   Also there is 32 KiB program memory and 1024 bytes of persistent
   memory. Those are not covered by this craft.
@@ -10,7 +10,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-05-24
+  Last mod.: 2024-10-03
 */
 
 #include <me_BaseTypes.h>
@@ -18,8 +18,6 @@
 #include <me_InstallStandardStreams.h>
 
 #include <me_MemorySegment.h>
-
-using namespace me_BaseTypes;
 
 // Forwards:
 void SendMemoryScan();
@@ -29,7 +27,7 @@ void setup()
   Serial.begin(me_UartSpeeds::Arduino_Normal_Bps);
   InstallStandardStreams();
   printf("[Memory explorer]: Okay, we are here.\n");
-  SendMemoryScan();
+  PrintMemory();
 }
 
 void loop()
@@ -40,28 +38,27 @@ void loop()
 void SendSegment(me_MemorySegment::TMemorySegment Segment);
 
 /*
-  Send memory state via Serial.
+  Print memory
 
   Memory range is hardcoded.
 */
-void SendMemoryScan()
+void PrintMemory()
 {
   /*
     Set memory range to transmit.
 
     Memory starts to repeat itself after offset (2048 + 256).
   */
-  me_MemorySegment::TMemorySegment Range =
-    {
-      .Start = { .Addr = 0 },
-      .Size = 2048 + 256,
-    };
+  me_MemorySegment::TMemorySegment Range;
+
+  Range.Start.Addr = 0;
+  Range.Size = 2048 + 256;
 
   SendSegment(Range);
 }
 
 /*
-  Send memory segment via Serial.
+  Print memory segment offset and contents
 
   Format sample: "[0x12EF .. +3]: 20 0A 0C \n"
 */
@@ -71,11 +68,12 @@ void SendSegment(me_MemorySegment::TMemorySegment Segment)
 
   for (TUint_2 Offset = 0; Offset < Segment.Size; ++Offset)
   {
-    printf("%02X ", Segment.Start.Bytes[Offset]);
+    printf("%02X ", Segment.Bytes[Offset]);
   }
   printf("\n");
 }
 
 /*
   2024-05-24
+  2024-10-03
 */
